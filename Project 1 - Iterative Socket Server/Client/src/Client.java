55be05bd07111a23d1cea67350c277859a3288ec
David Rojas <deznorth@gmail.com>
2021-02-28 15:25:18 -0500

    C2 - Update Client and share RequestTypes

--- Project 1 - Iterative Socket Server/Client/src/Client.java
+++ Project 1 - Iterative Socket Server/Client/src/Client.java
@@ -3,77 +3,81 @@ import java.rmi.UnknownHostException;
 import java.io.*;
 import java.util.Scanner;
 
+import entities.RequestType;
+
 public class Client {
-	
 	static Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		if(args.length < 1) {
-			return;
-		}
-		
-		String domainName = args[0];
-		
-		//Requests port number
+		// Variables
+		int PORT, nClients;
+		String HOST;
+
+		//Requests PORT number
 		System.out.print("Enter the port number: ");
-		int port = in.nextInt();
+		PORT = in.nextInt();
 		System.out.println();
-		
-		//Requests host name
+
+		//Requests HOST name
 		System.out.print("Enter the network address: ");
-		String host = in.next();
+		HOST = in.next();
 		System.out.println();
-		
-		try(Socket socket = new Socket(host, port)) {
-			
-			OutputStream output = socket.getOutputStream();
-			PrintWriter writer = new PrintWriter(output, true);
-			writer.println(domainName);
-			
-			InputStream input = socket.getInputStream();
-			
-			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-			
+
+		try (
+			Socket socket = new Socket(HOST, PORT);
+			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		) {
 			String line;
-			
-			while((line = reader.readLine()) != null) {
+
+			if (input.ready())
+			while((line = input.readLine()) != null) {
 				System.out.println(line);
 			}
-			
-		} catch(UnknownHostException ex) {
-			System.out.println("Server not found: " + ex.getMessage());
-			
-		} catch(IOException ex) {
-			System.out.println("I/O error: " + ex.getMessage());
-		}
-		
-		//Requests operation
-		System.out.print("\nList of operations: \n\n"
+
+			int operation;
+
+			do {
+				//Requests operation
+				System.out.print("\nList of operations: \n\n"
 				+ "1 - Date and Time\n"
 				+ "2 - Uptime\n"
 				+ "3 - Memory Use\n"
 				+ "4 - Netstat\n"
 				+ "5 - Current Users\n"
-				+ "6 - Running Processes\n\n"
-				+ "Enter the operation that you would like to request");
-		
-		int operation = in.nextInt();
-		System.out.println();
-		
-		while(operation != 7) {
-			
-			if(operation == 1) {
-				
-			}
-			
-			else {
-				System.out.println("Wrong command entered. Please try again.\n");
-			}
+				+ "6 - Running Processes\n"
+				+ "7 - Exit \n\n"
+				+ "Enter the operation that you would like to request: ");
+
+				operation = in.nextInt();
+				System.out.println();
+
+				switch (operation) {
+					case 1 : output.println(RequestType.DateTime.name());
+						break;
+					case 2 : output.println(RequestType.Uptime.name());
+						break;
+					case 3 : output.println(RequestType.Memory.name());
+						break;
+					case 4 : output.println(RequestType.Netstat.name());
+						break;
+					case 5 : output.println(RequestType.CurrentUsers.name());
+						break;
+					case 6 : output.println(RequestType.RunningProcesses.name());
+						break;
+					case 7 :
+						output.println(RequestType.Quit.name());
+						break;
+					default:
+						System.out.println("Wrong command entered. Please try again.\n");
+						break;
+				}
+			} while (operation != 7);
+		} catch(UnknownHostException ex) {
+			System.out.println("Server not found: " + ex.getMessage());
+		} catch(IOException ex) {
+			System.out.println("I/O error: " + ex.getMessage());
 		}
-
 	}
-
 }
 
---
+++ Project 1 - Iterative Socket Server/Client/src/entities/RequestType.java
@@ -0,0 +1,14 @@
+package entities;
+
+/**
+ * This Enum should be kept in sync with the client
+ */
+public enum RequestType {
+  DateTime,
+  Uptime,
+  Memory,
+  Netstat,
+  CurrentUsers,
+  RunningProcesses,
+  Quit,
+}
