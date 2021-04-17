99c21b33f62477051d75e2f0715d237db54bfdca
David Rojas <deznorth@gmail.com>
2021-04-17 19:34:29 -0400

    C9 - Copy P1 files over

--- Project 2 - Concurrent Socket Server/Client/src/Client.java
+++ Project 2 - Concurrent Socket Server/Client/src/Client.java
@@ -1,5 +1,121 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import entities.RequestType;
+import entities.cThread;
+import util.CSVExporter;
+
 public class Client {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello from the Client!");
-    }
+
+	static Scanner in = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// Variables
+		int PORT, nClients = 1, operation;
+		String HOST;
+
+		//Requests PORT number
+		System.out.print("Enter the port number: ");
+		PORT = in.nextInt();
+		System.out.println();
+
+		//Requests HOST name
+		System.out.print("Enter the network address: ");
+		HOST = in.next();
+		System.out.println();
+
+		do {
+			//Requests operation
+			System.out.print("\nList of operations: \n\n"
+			+ "1 - Date and Time\n"
+			+ "2 - Uptime\n"
+			+ "3 - Memory Use\n"
+			+ "4 - Netstat\n"
+			+ "5 - Current Users\n"
+			+ "6 - Running Processes\n"
+			+ "7 - Exit \n\n"
+			+ "Enter the operation that you would like to request: ");
+
+			operation = in.nextInt();
+			System.out.println();
+
+			if(operation > 0 && operation < 8) {
+				RequestType request = RequestType.DateTime; // Just so it is initialized
+				switch (operation) {
+					case 1 : request = RequestType.DateTime;
+						break;
+					case 2 : request = RequestType.Uptime;
+						break;
+					case 3 : request = RequestType.Memory;
+						break;
+					case 4 : request = RequestType.Netstat;
+						break;
+					case 5 : request = RequestType.CurrentUsers;
+						break;
+					case 6 : request = RequestType.RunningProcesses;
+						break;
+					case 7 : request = RequestType.Quit;
+						break;
+				}
+
+				if (request != RequestType.Quit) {
+					//Requests number of clients
+					System.out.print("\nEnter the number of clients to generate: ");
+					nClients = in.nextInt();
+					System.out.println("\n");
+				}
+
+				//Generates threads based on # of clients
+				cThread[] threads = new cThread[nClients];
+
+				for(int i = 0; i < nClients; i++) {
+					threads[i] = new cThread(HOST, PORT, i, request);
+				}
+
+				//Threads are executed
+				for (cThread thread : threads) {
+					thread.start();
+				}
+
+				// Join after all threads have started so the program waits for them to finish
+				for (cThread thread : threads) {
+					try {
+						thread.join();
+					} catch (InterruptedException e) {
+						System.out.println("Exception while trying to join thread\n" + e.getMessage());
+					}
+				}
+
+				if (request != RequestType.Quit) {
+					CSVExporter exporter = new CSVExporter();
+					ArrayList<String> col = new ArrayList<String>();
+
+					col.add(request.name());
+
+					// Calculate the average Server response time
+					long sumOfTimes = 0;
+					for (int i = 0; i< cThread.times.size(); i++) {
+						long t = cThread.times.get(i);
+						col.add(String.valueOf(t));
+						System.out.printf("%nTime for Client Request #%d: \t%dms", i + 1, t);
+						sumOfTimes += t;
+					}
+					double avgTime = sumOfTimes / (double)nClients;
+					col.add("");
+					col.add(String.valueOf(avgTime));
+					col.add(String.valueOf(sumOfTimes));
+					// Export values to csv file
+					exporter.export(col, String.format("%s_%s", request.name(), String.valueOf(nClients)));
+					cThread.times.clear();
+					System.out.printf("%n%nAverage time of response: \t%sms %n", avgTime);
+					System.out.printf("%nTotal turn around time: \t%sms %n", sumOfTimes);
+				}
+			} else {
+				System.out.println("Wrong command. Please try again\n");
+				continue;
+			}
+		} while (operation != 7);
+	}
 }
+
--- Project 2 - Concurrent Socket Server/Server/src/Server.java
+++ Project 2 - Concurrent Socket Server/Server/src/Server.java
@@ -1,5 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+import entities.RequestType;
+import handlers.*;
+
 public class Server {
-  public static void main(String[] args) {
-    System.out.println("Hello from the Server!");
-  }
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: Java Server <port number>");
+            System.exit(1);
+        }
+
+        // Variables
+        String PROJECT_NAME = "Project 1 - Iterative Socket Server";
+        int PORT = Integer.parseInt(args[0]);
+
+        // This Map holds all request handlers keyed by an enum so it's easy to add new handlers
+        HashMap<RequestType, RequestHandler> handlers = new HashMap<RequestType, RequestHandler> (Map.of(
+            RequestType.DateTime, new DateTimeHandler(),
+            RequestType.Uptime, new UptimeHandler(),
+            RequestType.Memory, new MemoryHandler(),
+            RequestType.Netstat, new NetstatHandler(),
+            RequestType.CurrentUsers, new CurrentUsersHandler(),
+            RequestType.RunningProcesses, new RunningProcessesHandler()
+        )) ;
+
+        // Greeting
+        System.out.printf("%s %n%nGroup:\tFernando Jimenez Mendez %n\tDavid Rojas Gonzalez %n%n", PROJECT_NAME);
+
+        try {
+            // Create listener bound to PORT with a max # of connections of 100
+            ServerSocket socket = new ServerSocket(PORT, 100);
+            System.out.println("Type CTRL+C to stop the server");
+
+            // Infinite loop
+            while (true) {
+                try (
+                    Socket client = socket.accept();
+                    PrintWriter output = new PrintWriter(client.getOutputStream(), true);
+                    BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                ) {
+                    RequestType requestType = RequestType.valueOf(input.readLine());
+
+                    if (requestType == RequestType.Quit) {
+                        break;
+                    }
+
+                    System.out.printf("%nHandling incoming '%s' request from [%s].%n", requestType.name(), client.getInetAddress());
+
+                    // Iterate the handlers HashMap and execute corresponding handler
+                    for (RequestType type : handlers.keySet()) {
+                        if (type == requestType) {
+                            String response = handlers.get(type).resolve() + "%n%n";
+                            // Submit to client
+                            output.printf(response);
+                        }
+                    }
+
+                    input.close();
+                    output.close();
+                    client.close();
+                } catch (IOException e) {
+                    System.err.println("Exception caught while handling a connection");
+                    System.out.println(e.getMessage());
+                }
+                System.out.println("Waiting for next connection...");
+            }
+
+            // Exit gracefully
+            socket.close();
+            System.exit(0);
+        } catch (IOException e) {
+            System.err.println("Exception caught while trying to listen on port " + PORT);
+            System.err.println(e.getMessage());
+        }
+    }
 }
