29a5d5347f7da9389855430545941ad2aca82735
David Rojas <deznorth@gmail.com>
2021-04-17 21:35:01 -0400

    C9 - Make server multi-threaded

--- Project 2 - Concurrent Socket Server/Server/src/Server.java
+++ Project 2 - Concurrent Socket Server/Server/src/Server.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
 
+import entities.HandlerThread;
 import entities.RequestType;
-import handlers.*;
 
 public class Server {
     public static void main(String[] args) throws IOException {
@@ -18,18 +15,8 @@ public class Server {
         }
 
         // Variables
-        String PROJECT_NAME = "Project 1 - Iterative Socket Server";
+        String PROJECT_NAME = "Project 2 - Concurrent Socket Server";
         int PORT = Integer.parseInt(args[0]);
-
-        // This Map holds all request handlers keyed by an enum so it's easy to add new handlers
-        HashMap<RequestType, RequestHandler> handlers = new HashMap<RequestType, RequestHandler> (Map.of(
-            RequestType.DateTime, new DateTimeHandler(),
-            RequestType.Uptime, new UptimeHandler(),
-            RequestType.Memory, new MemoryHandler(),
-            RequestType.Netstat, new NetstatHandler(),
-            RequestType.CurrentUsers, new CurrentUsersHandler(),
-            RequestType.RunningProcesses, new RunningProcessesHandler()
-        )) ;
 
         // Greeting
         System.out.printf("%s %n%nGroup:\tFernando Jimenez Mendez %n\tDavid Rojas Gonzalez %n%n", PROJECT_NAME);
@@ -41,36 +28,20 @@ public class Server {
 
             // Infinite loop
             while (true) {
-                try (
+                try {
                     Socket client = socket.accept();
-                    PrintWriter output = new PrintWriter(client.getOutputStream(), true);
                     BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                ) {
                     RequestType requestType = RequestType.valueOf(input.readLine());
 
                     if (requestType == RequestType.Quit) {
                         break;
                     }
 
-                    System.out.printf("%nHandling incoming '%s' request from [%s].%n", requestType.name(), client.getInetAddress());
-
-                    // Iterate the handlers HashMap and execute corresponding handler
-                    for (RequestType type : handlers.keySet()) {
-                        if (type == requestType) {
-                            String response = handlers.get(type).resolve() + "%n%n";
-                            // Submit to client
-                            output.printf(response);
-                        }
-                    }
-
-                    input.close();
-                    output.close();
-                    client.close();
+                    new HandlerThread(client, input, requestType).start();
                 } catch (IOException e) {
-                    System.err.println("Exception caught while handling a connection");
+                    System.err.println("Exception caught while starting a handler thread a connection");
                     System.out.println(e.getMessage());
                 }
-                System.out.println("Waiting for next connection...");
             }
 
             // Exit gracefully
